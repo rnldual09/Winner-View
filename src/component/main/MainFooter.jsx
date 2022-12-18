cc3b1501b0b9984560aa2728a6a9e6524ece42bd
rnldual09 <rnldual09@naver.com>
2022-12-18 18:10:25 +0900

    fetch test용도

--- src/component/main/MainFooter.jsx
+++ src/component/main/MainFooter.jsx
@@ -3,19 +3,19 @@ import { Image, View, Text, TouchableOpacity } from 'react-native';
 import styles from '../../style/styles';
 import axios from 'axios';
 
+
 const MainFooter = (props) => {
   
   const {item} = props;
 
   const fetchNotToken = async (  ) => {
-    
-    const url = '223.38.42.206:8080/common/postList.do';
-    const data = {'data1':'data_1','data2':'data_2','data3':'data_3'};
+  
+    const url = 'http://localhost:8080/post/postList.do';
     
     fetch(url, 
     {
-      method : 'GET',
-     
+      method : 'POST',
+      headers: {'Content-Type':'application/json'},
     }).then(function(response) {
       return response.json();                    
     }).then(function (responseText) {      
@@ -27,37 +27,10 @@ const MainFooter = (props) => {
     });
   };
 
-  const fetchNotToken2 = async (  ) => {
-    
-    const response = await axios.get('https://api.ipify.org?format=json');
-    console.log(response);
-  };
-
-  const fetchNotToken3 = async (  ) => {
-    
-    const url = 'https://www.taxicard.co.kr/AppTaxi/check/bid';
-    const data = {'bidNo':'3122322172'};
-
-    fetch(url, 
-    {
-      method : 'POST',
-      body : JSON.stringify(data),
-      headers: { 'Content-Type': 'application/json' }
-    }).then(function(response) {
-      return response.json();                    
-    }).then(function (responseText) {      
-      console.log(responseText);
-    }).catch(function(err){
-      console.log(err);
-    });
-  };
-
-
   return (    
     <View style={styles().MainFooterContainer}>
       <TouchableOpacity
         style={styles().MainFooterButton}
-        onPress={() => fetchNotToken3()}
       >
         <Image 
           source={require('../../assets/icon/newRegist.png')}
@@ -67,7 +40,6 @@ const MainFooter = (props) => {
       </TouchableOpacity>
       <TouchableOpacity
         style={styles().MainFooterButton}
-        onPress={() => fetchNotToken2()}
       >
         <Image 
           source={require('../../assets/icon/message.png')}
